fec114f6d38203fd0159c77411a185a4770c5e64
SHIHAIFENG15 <980627yxz@gmail.com>
2026-08-21 10:14:25 +0800

    [Test] Fold draft export device tests; keep ExportDB GENERIC
    Address review: one TestDraftExportDevice for memory + meta-kernel
    guard, skipIf for accelerator memory, and drop device-agnostic
    instantiate from test_db.py.
    
    Test Plan:
    ```
    python test/export/test_draft_export.py -v -k TestDraftExportDevice
    python test/export/test_db.py -v
    ```

--- test/export/test_db.py
+++ test/export/test_db.py
@@ -3,9 +3,7 @@
 import copy
 import unittest
 
-import torch
 import torch._dynamo as torchdynamo
-import torch.utils._pytree as pytree
 from torch._export import config
 from torch._export.db.case import ExportCase, SupportLevel
 from torch._export.db.examples import (
@@ -13,9 +11,9 @@ from torch._export.db.examples import (
     get_rewrite_cases,
 )
 from torch.export import export
-from torch.testing._internal.common_device_type import instantiate_device_type_tests
 from torch.testing._internal.common_utils import (
     HardwareClassification,
+    instantiate_parametrized_tests,
     IS_WINDOWS,
     parametrize,
     run_tests,
@@ -23,17 +21,10 @@ from torch.testing._internal.common_utils import (
 )
 
 
-_CPU_BOUND_EXPORTDB_CASES = frozenset({"optional_input", "static_if"})
-
-
-def _to_device(obj, device):
-    return pytree.tree_map_only(torch.Tensor, lambda x: x.to(device), obj)
-
-
 @unittest.skipIf(IS_WINDOWS, "Windows not supported for this test")
 @unittest.skipIf(not torchdynamo.is_dynamo_supported(), "dynamo doesn't support")
 class ExampleTests(TestCase):
-    hw_classification = HardwareClassification.ACCELERATOR
+    hw_classification = HardwareClassification.GENERIC
 
     # TODO Maybe we should make this tests actually show up in a file?
     @parametrize(
@@ -41,22 +32,18 @@ class ExampleTests(TestCase):
         filter_examples_by_support_level(SupportLevel.SUPPORTED).items(),
         name_fn=lambda name, case: f"case_{name}",
     )
-    def test_exportdb_supported(self, device, name: str, case: ExportCase) -> None:
-        if name in _CPU_BOUND_EXPORTDB_CASES and torch.device(device).type != "cpu":
-            self.skipTest(f"{name} constructs CPU tensors in the example")
-        model = copy.deepcopy(case.model)
-        if isinstance(model, torch.nn.Module):
-            model = model.to(device)
+    def test_exportdb_supported(self, name: str, case: ExportCase) -> None:
+        model = case.model
 
-        args_export = _to_device(copy.deepcopy(case.example_args), device)
-        kwargs_export = _to_device(copy.deepcopy(case.example_kwargs), device)
+        args_export = case.example_args
+        kwargs_export = case.example_kwargs
         args_model = copy.deepcopy(args_export)
         kwargs_model = copy.deepcopy(kwargs_export)
         with config.patch(use_new_tracer_experimental=True):
             exported_program = export(
                 model,
-                args_export,
-                kwargs_export,
+                case.example_args,
+                case.example_kwargs,
                 dynamic_shapes=case.dynamic_shapes,
                 strict=True,
             )
@@ -68,7 +55,7 @@ class ExampleTests(TestCase):
         )
 
         if case.extra_args is not None:
-            args = _to_device(copy.deepcopy(case.extra_args), device)
+            args = case.extra_args
             args_model = copy.deepcopy(args)
             self.assertEqual(
                 exported_program.module()(*args),
@@ -80,12 +67,8 @@ class ExampleTests(TestCase):
         filter_examples_by_support_level(SupportLevel.NOT_SUPPORTED_YET).items(),
         name_fn=lambda name, case: f"case_{name}",
     )
-    def test_exportdb_not_supported(self, device, name: str, case: ExportCase) -> None:
-        model = copy.deepcopy(case.model)
-        if isinstance(model, torch.nn.Module):
-            model = model.to(device)
-        args = _to_device(copy.deepcopy(case.example_args), device)
-        kwargs = _to_device(copy.deepcopy(case.example_kwargs), device)
+    def test_exportdb_not_supported(self, name: str, case: ExportCase) -> None:
+        model = case.model
         # pyre-ignore
         with self.assertRaises(
             (torchdynamo.exc.Unsupported, AssertionError, RuntimeError)
@@ -93,8 +76,8 @@ class ExampleTests(TestCase):
             with config.patch(use_new_tracer_experimental=True):
                 _ = export(
                     model,
-                    args,
-                    kwargs,
+                    case.example_args,
+                    case.example_kwargs,
                     dynamic_shapes=case.dynamic_shapes,
                     strict=True,
                 )
@@ -114,24 +97,19 @@ class ExampleTests(TestCase):
             name_fn=lambda name, case: f"case_{name}_{case.name}",
         )
         def test_exportdb_not_supported_rewrite(
-            self, device, name: str, rewrite_case: ExportCase
+            self, name: str, rewrite_case: ExportCase
         ) -> None:
-            model = copy.deepcopy(rewrite_case.model)
-            if isinstance(model, torch.nn.Module):
-                model = model.to(device)
-            args = _to_device(copy.deepcopy(rewrite_case.example_args), device)
-            kwargs = _to_device(copy.deepcopy(rewrite_case.example_kwargs), device)
             # pyre-ignore
             export(
-                model,
-                args,
-                kwargs,
+                rewrite_case.model,
+                rewrite_case.example_args,
+                rewrite_case.example_kwargs,
                 dynamic_shapes=rewrite_case.dynamic_shapes,
                 strict=True,
             )
 
 
-instantiate_device_type_tests(ExampleTests, globals())
+instantiate_parametrized_tests(ExampleTests)
 
 
 if __name__ == "__main__":
