662ded54fae380cbd28972470698405216973849
Bob Ren <bobren@meta.com>
2026-08-22 12:30:28 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/output_graph.py
+++ torch/_dynamo/output_graph.py
@@ -2604,7 +2604,7 @@ class OutputGraph(OutputGraphCommon):
                 **kwargs,
             },
         )
-        self.package.bypass_current_entry()
+        self.package.bypass_current_entry(reason)
         self.package = None
 
     def get_graph_sizes_structured(self) -> dict[str, list[int | str]]:
