14976dddfd1ce12696ab89d075e599aa1cec3d26
drisspg <drisspguessous@gmail.com>
2026-08-25 15:48:34 -0700

    Update
    [ghstack-poisoned]

--- torch/nn/attention/varlen.py
+++ torch/nn/attention/varlen.py
@@ -89,6 +89,8 @@ def _cudnn_rejection_reasons(
         reasons.append("query dtype must be float16 or bfloat16")
     if query.shape[-1] % 8 != 0 or value.shape[-1] % 8 != 0:
         reasons.append("query and value head dimensions must be divisible by 8")
+    if query.shape[-1] > 128 or value.shape[-1] > 128:
+        reasons.append("head dimensions must be at most 128")
     if window_size == [-1, 0]:
         if cu_seq_q is not cu_seq_k:
             reasons.append(
@@ -270,7 +272,16 @@ def _varlen_attn_fake(
     """
     window_size = _normalize_window_size(window_size)
 
-    output = _empty_with_matching_layout(query, (*query.shape[:-1], value.size(-1)))
+    if backend == _CUDNN_ATTENTION_BACKEND:
+        # The cuDNN varlen forward always allocates a contiguous output
+        # (aten/src/ATen/native/cudnn/MHA.cpp), regardless of query layout.
+        output = torch.empty(
+            (*query.shape[:-1], value.size(-1)),
+            dtype=query.dtype,
+            device=query.device,
+        )
+    else:
+        output = _empty_with_matching_layout(query, (*query.shape[:-1], value.size(-1)))
 
     # For varlen path: logsumexp shape is (num_heads, total_q)
     total_q = query.size(0)
