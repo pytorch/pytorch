b6d990222d62518d03219676d0ec5bca6aff6804
Piotr Bielak <piotr.bielak@intel.com>
2026-08-26 14:54:20 +0000

    Extract class for device-unrelated TestScatterGather tests

--- test/test_scatter_gather_ops.py
+++ test/test_scatter_gather_ops.py
@@ -13,7 +13,6 @@ from torch.testing._internal.common_cuda import (
 )
 from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
-    onlyCPU,
     onlyAccelerator,
     dtypes,
     dtypesIfCUDA,
@@ -46,6 +45,109 @@ if torch.get_default_dtype() is not torch.float32:
 # Note: test_scatter_gather_ops.py
 # This test file tests scatter and gather operations,
 #   like torch.scatter and torch.gather.
+
+class TestScatterGather(TestCase):
+    @parametrize("dtype", [torch.float32, torch.float64, torch.bfloat16, torch.float16])
+    def test_scatter_expanded_index(self, dtype):
+        device = "cpu"
+
+        def helper(input_size, idx_size):
+            input = torch.randn(input_size, device=device).to(dtype=dtype)
+            input2 = input.clone()
+
+            shape = [1] * len(input_size)
+            shape[0] = idx_size
+            dim_size = input_size[0]
+            idx = torch.randint(0, dim_size, shape)
+
+            # The fast path on scatter when index is expanded
+            # will depend on sorted index where the collected src indice
+            # for each row in input will be mapped to rowptrs in a CSR format.
+            # Create some empty rows by masking:
+            mask = (idx > 1) * (idx < 4)
+            idx[mask] = 0
+
+            expanded_shape = input_size
+            expanded_shape[0] = idx_size
+            idx = idx.expand(expanded_shape)
+            idx2 = idx.contiguous()
+            src = torch.randn(expanded_shape, device=device).to(dtype=dtype)
+
+            out = input.scatter_add(0, idx, src)
+            out2 = input2.scatter_add(0, idx2, src)
+            self.assertEqual(out, out2)
+
+            for reduce in ["sum", "prod", "mean", "amax", "amin"]:
+                for include_self in [True, False]:
+                    out = input.scatter_reduce(0, idx, src, reduce=reduce, include_self=include_self)
+                    out2 = input2.scatter_reduce(0, idx2, src, reduce=reduce, include_self=include_self)
+                    self.assertEqual(out, out2)
+
+        helper([50, 17], 100)
+        helper([50, 1], 100)
+        helper([50, 8, 7], 100)
+        helper([50, 3, 4, 5], 100)
+
+    @parametrize("dtype", [torch.float32, torch.float64, torch.bfloat16])
+    def test_gather_expanded_index(self, dtype):
+        device = "cpu"
+
+        # Test when index is [N, 1], which would have stride [1, 0]
+        # should be excluded from the fast path when index ix expanded
+        input = torch.arange(25).view(5, 5)
+        input2 = input.to(dtype=dtype)
+
+        idx = torch.arange(5).view(5, 1)
+        out = torch.gather(input, 0, idx)
+        out2 = torch.gather(input2, 0, idx)
+
+        self.assertEqual(out.to(dtype=dtype), out2)
+
+        def helper(input_size, idx_size):
+            input = torch.randn(input_size, device=device).to(dtype=dtype)
+            input2 = input.clone()
+
+            shape = [1] * len(input_size)
+            shape[0] = idx_size
+            dim_size = input_size[0]
+            idx = torch.randint(0, dim_size, shape)
+
+            # Test the fast path on gather when index is expanded
+            expanded_shape = input_size
+            expanded_shape[0] = idx_size
+            idx = idx.expand(expanded_shape)
+            idx2 = idx.contiguous()
+
+            out = torch.gather(input, 0, idx)
+            out2 = torch.gather(input2, 0, idx2)
+
+            self.assertEqual(out, out2)
+
+            # test unsqueezed index
+            # expanded_index kernel can not handle the case:
+            # the size > 1 and stride == 1 at a dimension.
+            # for example: the index with size of [1, 8, 7],  stride of [1, 1, 0].
+            # see https://github.com/pytorch/pytorch/issues/129093
+            def unsqueeze_helper(idx, dim):
+                if dim == 2:
+                    return idx.unsqueeze(1).t()
+                else:
+                    return unsqueeze_helper(idx, dim - 1).unsqueeze(dim - 1)
+
+            idx = torch.randint(0, dim_size, (input.shape[1],))
+            idx = unsqueeze_helper(idx, len(input_size))
+            expanded_shape[0] = 1
+            idx = idx.expand(expanded_shape)
+            idx2 = idx.contiguous()
+            out = torch.gather(input, 0, idx)
+            out2 = torch.gather(input2, 0, idx2)
+            self.assertEqual(out, out2)
+
+        helper([50, 17], 100)
+        helper([50, 1], 100)
+        helper([50, 8, 7], 100)
+        helper([50, 3, 4, 5], 100)
+
 
 class TestScatterGatherDevice(TestCase):
     # Fills an index tensor with valid indices
@@ -470,46 +572,6 @@ class TestScatterGatherDevice(TestCase):
                     expected_result[2] = 0
                 self.assertEqual(input, expected_result)
 
-    @onlyCPU
-    @dtypes(torch.float32, torch.float64, torch.bfloat16, torch.float16)
-    def test_scatter_expanded_index(self, device, dtype):
-        def helper(input_size, idx_size):
-            input = torch.randn(input_size, device=device).to(dtype=dtype)
-            input2 = input.clone()
-
-            shape = [1] * len(input_size)
-            shape[0] = idx_size
-            dim_size = input_size[0]
-            idx = torch.randint(0, dim_size, shape)
-
-            # The fast path on scatter when index is expanded
-            # will depend on sorted index where the collected src indice
-            # for each row in input will be mapped to rowptrs in a CSR format.
-            # Create some empty rows by masking:
-            mask = (idx > 1) * (idx < 4)
-            idx[mask] = 0
-
-            expanded_shape = input_size
-            expanded_shape[0] = idx_size
-            idx = idx.expand(expanded_shape)
-            idx2 = idx.contiguous()
-            src = torch.randn(expanded_shape, device=device).to(dtype=dtype)
-
-            out = input.scatter_add(0, idx, src)
-            out2 = input2.scatter_add(0, idx2, src)
-            self.assertEqual(out, out2)
-
-            for reduce in ["sum", "prod", "mean", "amax", "amin"]:
-                for include_self in [True, False]:
-                    out = input.scatter_reduce(0, idx, src, reduce=reduce, include_self=include_self)
-                    out2 = input2.scatter_reduce(0, idx2, src, reduce=reduce, include_self=include_self)
-                    self.assertEqual(out, out2)
-
-        helper([50, 17], 100)
-        helper([50, 1], 100)
-        helper([50, 8, 7], 100)
-        helper([50, 3, 4, 5], 100)
-
     @dtypes(torch.float32)
     def test_scatter_add_broadcasted_index_deterministic(self, device, dtype):
         for d in (0, 1):
@@ -526,65 +588,6 @@ class TestScatterGatherDevice(TestCase):
                 res = inp.clone().scatter_add_(d, idx, src)
             self.assertEqual(res, ref)
 
-
-    @onlyCPU
-    @dtypes(torch.float32, torch.float64, torch.bfloat16)
-    def test_gather_expanded_index(self, device, dtype):
-        # Test when index is [N, 1], which would have stride [1, 0]
-        # should be excluded from the fast path when index ix expanded
-        input = torch.arange(25).view(5, 5)
-        input2 = input.to(dtype=dtype)
-
-        idx = torch.arange(5).view(5, 1)
-        out = torch.gather(input, 0, idx)
-        out2 = torch.gather(input2, 0, idx)
-
-        self.assertEqual(out.to(dtype=dtype), out2)
-
-        def helper(input_size, idx_size):
-            input = torch.randn(input_size, device=device).to(dtype=dtype)
-            input2 = input.clone()
-
-            shape = [1] * len(input_size)
-            shape[0] = idx_size
-            dim_size = input_size[0]
-            idx = torch.randint(0, dim_size, shape)
-
-            # Test the fast path on gather when index is expanded
-            expanded_shape = input_size
-            expanded_shape[0] = idx_size
-            idx = idx.expand(expanded_shape)
-            idx2 = idx.contiguous()
-
-            out = torch.gather(input, 0, idx)
-            out2 = torch.gather(input2, 0, idx2)
-
-            self.assertEqual(out, out2)
-
-            # test unsqueezed index
-            # expanded_index kernel can not handle the case:
-            # the size > 1 and stride == 1 at a dimension.
-            # for example: the index with size of [1, 8, 7],  stride of [1, 1, 0].
-            # see https://github.com/pytorch/pytorch/issues/129093
-            def unsqueeze_helper(idx, dim):
-                if dim == 2:
-                    return idx.unsqueeze(1).t()
-                else:
-                    return unsqueeze_helper(idx, dim - 1).unsqueeze(dim - 1)
-
-            idx = torch.randint(0, dim_size, (input.shape[1],))
-            idx = unsqueeze_helper(idx, len(input_size))
-            expanded_shape[0] = 1
-            idx = idx.expand(expanded_shape)
-            idx2 = idx.contiguous()
-            out = torch.gather(input, 0, idx)
-            out2 = torch.gather(input2, 0, idx2)
-            self.assertEqual(out, out2)
-
-        helper([50, 17], 100)
-        helper([50, 1], 100)
-        helper([50, 8, 7], 100)
-        helper([50, 3, 4, 5], 100)
 
 # ---------------------------------------------------------------------------
 # CuTeDSL scatter_add override tests. Two surfaces:
@@ -1195,6 +1198,7 @@ class TestScatterAddOverrideCorrectness(TestCase):
 instantiate_parametrized_tests(TestScatterAddOverrideConds)
 instantiate_parametrized_tests(TestScatterAddOverrideCorrectness)
 
+instantiate_parametrized_tests(TestScatterGather)
 instantiate_device_type_tests(TestScatterGatherDevice, globals())
 
 if __name__ == '__main__':
