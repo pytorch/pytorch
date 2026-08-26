22da0ab46ca2502cf09fa82f79e2d15d9ff926f5
zhaiyaxing001 <616636986@qq.com>
2026-08-26 19:00:40 +0800

    add allow_xpu=True

--- test/distributed/algorithms/test_join.py
+++ test/distributed/algorithms/test_join.py
@@ -525,7 +525,7 @@ class TestJoin(MultiProcessTestCase):
             device=device,
         )
 
-instantiate_device_type_tests(TestJoin, globals(), except_for="cpu")
+instantiate_device_type_tests(TestJoin, globals(), except_for="cpu", allow_xpu=True)
 
 if __name__ == "__main__":
     run_tests()
