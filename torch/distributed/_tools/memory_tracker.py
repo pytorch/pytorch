86cbab25f175ae56819c26da6c4332806a5ba90c
Stalin <161853795+0x5t4l1n@users.noreply.github.com>
2026-08-28 00:10:27 +0530

    rebase onto current main

--- torch/distributed/_tools/memory_tracker.py
+++ torch/distributed/_tools/memory_tracker.py
@@ -103,6 +103,9 @@ class MemoryTracker:
             # the memory stats tracked here may not completely accurate.
             h1 = m.register_forward_pre_hook(self._create_pre_forward_hook(name))
             h2 = m.register_forward_hook(self._create_post_forward_hook(name))
+            # it does not work well with jagged tensor somehow, the root cause is not
+            # clear and remove it for now as it does not really capture important info.
+            # h3 = m.register_backward_hook(self._create_backward_hook(name))
             self._hooks.extend([h1, h2])
         self._device_module.empty_cache()
         if getattr(self, "profile_mode", None) is not None:
@@ -190,6 +193,8 @@ class MemoryTracker:
         y_2 = [gb for (name, gb) in self.memories_active.values()]
         y_3 = [gb for (name, gb) in self.memories_reserved.values()]
         x = list(range(len(y_1)))
+        # Split figures when there is big difference between
+        # "reserved_memory" and "allocated_memory" or "active_memory".
         _plot_figure(
             x,
             [list(y_1), list(y_2), list(y_3)],
