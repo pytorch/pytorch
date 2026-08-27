702c3473b458d41519c89db90d6dcf4dc30c068d
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2026-08-27 17:08:00 -0500

    Remove ROCm thread count adjustment
    Remove ROCm specific thread count adjustment from script.

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -206,10 +206,6 @@ if [[ -z "${OMP_NUM_THREADS:-}" ]] && [[ -n "${USE_ARC:-}" ]]; then
   export OMP_NUM_THREADS
 fi
 
-if [[ "$BUILD_ENVIRONMENT" == *rocm* ]]; then
-  export OMP_NUM_THREADS=$(( $(nproc) - 2 ))
-fi
-
 export LANG=C.UTF-8
 
 PR_NUMBER=${PR_NUMBER:-${CIRCLE_PR_NUMBER:-}}
