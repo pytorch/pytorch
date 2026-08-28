972de5587e4de2f3061022928c7e24ba006668b5
Isalia20 <irakli.salia854@gmail.com>
2026-08-28 19:04:07 +0400

    port nll_loss backward to metal

--- aten/src/ATen/native/mps/kernels/LossOps.h
+++ aten/src/ATen/native/mps/kernels/LossOps.h
@@ -1,6 +1,22 @@
 #pragma once
 #include <c10/metal/common.h>
 
+struct NLLLossBackwardParams {
+  int64_t n_classes;
+  int64_t map_size;
+  int64_t batch_stride;
+  int64_t class_stride;
+  int64_t grad_input_offset;
+  int64_t grad_output_offset;
+  int64_t target_offset;
+  int64_t weight_offset;
+  int64_t total_weight_offset;
+  int64_t ignore_index;
+  int64_t tid_offset;
+  uint32_t reduction;
+  uint32_t has_weight;
+};
+
 template <typename index_t>
 struct CTCLossParams {
   index_t BLANK;
