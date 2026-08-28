362d883f118a8b5d04728afa6c4db764f632c304
taochong <2636757099@qq.com>
2026-08-28 11:46:56 +0800

    [Testcase Refactoring] Match dict test class attribute spacing
    Signed-off-by: taochong <2636757099@qq.com>

--- test/dynamo/test_dicts.py
+++ test/dynamo/test_dicts.py
@@ -3341,11 +3341,13 @@ class DictMethodsTests(torch._dynamo.test_case.TestCase):
 
 class DictSubclassMethodsTests(DictMethodsTests):
     hw_classification = HardwareClassification.GENERIC
+
     thetype = SimpleDict
 
 
 class OrderedDictMethodsTests(DictMethodsTests):
     hw_classification = HardwareClassification.GENERIC
+
     thetype = OrderedDict
 
     # Methods:
