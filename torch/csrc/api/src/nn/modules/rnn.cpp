f63d5a71a7e54fa388a27c2815006434d40bce55
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-08-29 13:29:48 -0700

    Fix another typo

--- torch/csrc/api/src/nn/modules/rnn.cpp
+++ torch/csrc/api/src/nn/modules/rnn.cpp
@@ -163,8 +163,8 @@ void RNNImplBase<Derived>::reset() {
   flat_weights_ = {};
   for (const auto& wn : flat_weights_names_) {
     auto named_parameters = this->named_parameters(/*recurse=*/false);
-    if (auto it = named_parameters.find(wn); it != named_parameters.end()) {
-      flat_weights_.emplace_back(it->value());
+    if (auto* parameter = named_parameters.find(wn)) {
+      flat_weights_.emplace_back(*parameter);
     } else {
       flat_weights_.emplace_back();
     }
@@ -242,8 +242,8 @@ void RNNImplBase<Derived>::reset_flat_weights() {
   flat_weights_ = {};
   for (const auto& wn : flat_weights_names_) {
     auto named_parameters = this->named_parameters(/*recurse=*/false);
-    if (auto it = named_parameters.find(wn); it != named_parameters.end()) {
-      flat_weights_.emplace_back(it->value());
+    if (auto* parameter = named_parameters.find(wn)) {
+      flat_weights_.emplace_back(*parameter);
     } else {
       flat_weights_.emplace_back();
     }
