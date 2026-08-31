9cf974a515de865628d8cca9aad3533fb50322dd
Frank Lin <eee4017@gmail.com>
2026-08-31 23:44:06 +0000

    Update
    [ghstack-poisoned]

--- c10/cuda/CUDAGraphMemory.cpp
+++ c10/cuda/CUDAGraphMemory.cpp
@@ -58,7 +58,7 @@ size_t CaptureTracker::captureEnd(CaptureId_t capture_id) {
   return ended_capture.invalid_capture_free_count;
 }
 
-AllocationContext CaptureTracker::allocationContextSlow(
+AllocationContext CaptureTracker::allocationContext(
     cudaStream_t request_stream) const {
   const auto info = c10::cuda::captureInfoMayInitCtx(request_stream);
   AllocationContext context{info.status != CaptureStatus::None, std::nullopt};
@@ -80,7 +80,9 @@ AllocationContext CaptureTracker::allocationContextSlow(
 void CaptureTracker::recordAllocation(
     const void* block,
     const AllocationContext& context) {
-  TORCH_INTERNAL_ASSERT(context.tracked_capture_id.has_value());
+  if (!context.tracked_capture_id.has_value()) {
+    return;
+  }
   block_allocation_captures_.insert_or_assign(
       block, *context.tracked_capture_id);
 }
--- c10/cuda/impl/CUDAGraphMemory.h
+++ c10/cuda/impl/CUDAGraphMemory.h
@@ -23,12 +23,7 @@ class CaptureTracker {
   bool hasActiveCaptures() const {
     return active_captures_ != 0;
   }
-  AllocationContext allocationContext(cudaStream_t request_stream) const {
-    if (C10_LIKELY(active_captures_ == 0)) {
-      return {};
-    }
-    return allocationContextSlow(request_stream);
-  }
+  AllocationContext allocationContext(cudaStream_t request_stream) const;
   void recordAllocation(const void* block, const AllocationContext& context);
   void recordFree(const void* block, cudaStream_t free_stream);
 
@@ -45,7 +40,6 @@ class CaptureTracker {
   bool isFreeInAllocationCaptureOrAncestor(
       CaptureId_t allocation_capture_id,
       CaptureId_t free_capture_id) const;
-  AllocationContext allocationContextSlow(cudaStream_t request_stream) const;
   int eraseCaptureTree(CaptureId_t root_capture_id);
 
   int active_captures_{0};
