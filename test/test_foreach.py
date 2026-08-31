981a904040527f34d00bc7ff5136e04ece03fc9d
Piotr Bielak <piotr.bielak@intel.com>
2026-08-31 13:45:36 +0000

    Refactor `test_foreach_copy_with_multi_device_inputs` to use `deviceCountAtLeast(2)`

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -1601,13 +1601,14 @@ class TestForeachDevice(TestCase):
             torch.foreach.add(tensors, torch.tensor([1.0, 1.0], device=device))
 
     @onlyAccelerator
+    @deviceCountAtLeast(2)
     @ops(filter(lambda op: op.name == "_foreach_copy", foreach_binary_op_db))
-    def test_foreach_copy_with_multi_device_inputs(self, device, dtype, op):
+    def test_foreach_copy_with_multi_device_inputs(self, devices, dtype, op):
         foreach_copy_ = op.inplace_variant
         copy_ = op.ref_inplace
         for non_blocking in (False, True):
             for sample in op.sample_inputs(
-                device, dtype, noncontiguous=False, allow_higher_dtype_scalars=True
+                devices[0], dtype, noncontiguous=False, allow_higher_dtype_scalars=True
             ):
                 with torch.no_grad():
                     ref_input = [t.detach().clone() for t in sample.input]
@@ -1615,13 +1616,11 @@ class TestForeachDevice(TestCase):
                 for t, s in zip(ref_input, sample.args[0]):
                     copy_(t, s, non_blocking)
                 self.assertEqual(sample.input, ref_input)
-                if torch.accelerator.device_count() > 1:
-                    device = torch.device(self.device_type, 1)
-                    rhs_tensors = [t.to(device) for t in sample.args[0]]
-                    foreach_copy_(sample.input, rhs_tensors, non_blocking=non_blocking)
-                    for t, s in zip(ref_input, rhs_tensors):
-                        copy_(t, s, non_blocking)
-                    self.assertEqual(ref_input, sample.input)
+                rhs_tensors = [t.to(devices[1]) for t in sample.args[0]]
+                foreach_copy_(sample.input, rhs_tensors, non_blocking=non_blocking)
+                for t, s in zip(ref_input, rhs_tensors):
+                    copy_(t, s, non_blocking)
+                self.assertEqual(ref_input, sample.input)
 
     @onlyAccelerator
     @ops(filter(lambda op: op.name == "_foreach_copy", foreach_binary_op_db))
