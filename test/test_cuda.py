2d18a8175a377dd55fd4e1f2a9eacdf36f2650f1
PyTorch MergeBot <pytorchmergebot@users.noreply.github.com>
2026-09-02 23:23:43 +0000

    Revert "[ROCm] Report SDK and HIP versions separately (#193602)"
    This reverts commit c9b4d84a18dcc49ce1a26394321e8f9d36d46050.
    
    Reverted https://github.com/pytorch/pytorch/pull/193602 on behalf of https://github.com/huydhn due to Sorry for the churn, I need to revert this to cleanly revert https://github.com/pytorch/pytorch/pull/193927 ([comment](https://github.com/pytorch/pytorch/pull/193602#issuecomment-5517829946))

--- test/test_cuda.py
+++ test/test_cuda.py
@@ -44,7 +44,6 @@ from torch.testing._internal.common_cuda import (
     has_device_side_assert,
     PLATFORM_SUPPORTS_GREEN_CONTEXT,
     PLATFORM_SUPPORTS_WORKQUEUE_CONFIG,
-    ROCM_VERSION,
     SM70OrLater,
     SM89OrLater,
     TEST_CUDNN,
@@ -880,6 +879,7 @@ print(t.is_pinned())
             else:
                 # ROCm logic is less so, it's cublaslt for some Instinct, cublas for all else
                 # Mirror CUDAHooks::getHipblasltPreferredArchs in CUDAHooks.cpp
+                ROCM_VERSION = tuple(int(v) for v in torch.version.hip.split(".")[:2])
                 archs = ["gfx90a", "gfx942", "gfx1200", "gfx1201", "gfx950"]
                 if ROCM_VERSION >= (7, 13):
                     archs.extend(["gfx1100", "gfx1101", "gfx1151"])
--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -2577,9 +2577,10 @@ def skipIfHpu_BUGGY(fn):
             fn(*args, **kwargs)
     return wrapper
 
-def getRocmVersion() -> tuple[int, ...]:
+def getRocmVersion() -> tuple[int, int]:
     from torch.testing._internal.common_cuda import _get_torch_rocm_version
-    return _get_torch_rocm_version()
+    rocm_version = _get_torch_rocm_version()
+    return (rocm_version[0], rocm_version[1])
 
 # Skips a test on CUDA if ROCm is available and its version is lower than requested.
 def skipIfRocmVersionLessThan(version=None):
