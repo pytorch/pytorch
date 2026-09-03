d6ddc69797a10de31a2d4bb326bcc0dc09424286
Chris Leonard <chleonar@redhat.com>
2026-09-03 18:28:42 +0000

    updated migration to be minimal in order to eventually get vec128 over to headeronly. removed complex_math.cpp and everything in there is just in the headeronly file now.

--- c10/util/complex_math.h
+++ c10/util/complex_math.h
@@ -3,18 +3,4 @@
     "c10/util/complex_math.h is not meant to be individually included. Include c10/util/complex.h instead."
 #endif
 
-#include <c10/macros/Macros.h>
-
-#if defined(_LIBCPP_VERSION) || \
-    (defined(__GLIBCXX__) && !defined(_GLIBCXX11_USE_C99_COMPLEX))
-namespace c10_complex_math {
-namespace _detail {
-C10_API c10::complex<float> sqrt(const c10::complex<float>& in);
-C10_API c10::complex<double> sqrt(const c10::complex<double>& in);
-C10_API c10::complex<float> acos(const c10::complex<float>& in);
-C10_API c10::complex<double> acos(const c10::complex<double>& in);
-} // namespace _detail
-} // namespace c10_complex_math
-#endif
-
 #include <torch/headeronly/util/complex_math.h> // IWYU pragma: keep
