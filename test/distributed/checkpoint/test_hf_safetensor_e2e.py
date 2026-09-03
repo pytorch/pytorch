a3e02bcd30a6024f7eb3e1908e6345905c6d1ac5
daniel <danieldale2026@gmail.com>
2026-09-03 15:02:45 +0800

    [DCP] Restore accelerator test instantiation opt-in

--- test/distributed/checkpoint/test_hf_safetensor_e2e.py
+++ test/distributed/checkpoint/test_hf_safetensor_e2e.py
@@ -625,16 +625,19 @@ instantiate_device_type_tests(
     TestDistributedHFSafetensorsConsolidation,
     globals(),
     except_for=("cpu",),
+    allow_xpu=True,
 )
 instantiate_device_type_tests(
     TestDTensorReshardPlacementChange,
     globals(),
     except_for=("cpu",),
+    allow_xpu=True,
 )
 instantiate_device_type_tests(
     TestDTensorReshardMeshChange,
     globals(),
     except_for=("cpu",),
+    allow_xpu=True,
 )
 
 
