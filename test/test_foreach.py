ef61e301ead27fedb8bbf0ff5d3be206bcfc6dfa
Piotr Bielak <piotr.bielak@intel.com>
2026-09-03 12:27:15 +0000

    Move `test_foreach_scalarlist_complex_double_many_tensors` into `TestForeachDevice`

--- test/test_foreach.py
+++ test/test_foreach.py
@@ -21,7 +21,6 @@ from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
     largeTensorTest,
     onlyAccelerator,
-    onlyCUDA,
     OpDTypes,
     ops,
     skipCUDAIf,
@@ -1133,6 +1132,29 @@ class TestForeachDevice(TestCase):
             self.assertEqual(expect, actual, equal_nan=True)
 
     @onlyAccelerator
+    @dtypes(torch.complex128)
+    def test_foreach_scalarlist_complex_double_many_tensors(self, device, dtype):
+        # Prevent regressions for complex double MTA chunking, see #189827
+        N = 600
+        scalars = [i + 2.0 for i in range(N)]
+
+        def make_tensors():
+            return [
+                torch.full((1, 1, 1), i + 1, dtype=dtype, device=device)
+                for i in range(N)
+            ]
+
+        # out-of-place: depth 2
+        tensors = make_tensors()
+        expect = [t / s for t, s in zip(tensors, scalars)]
+        self.assertEqual(torch.foreach.div(tensors, scalars), expect)
+
+        # in-place: depth 1
+        tensors = make_tensors()
+        torch.foreach.div_(tensors, scalars)
+        self.assertEqual(tensors, expect)
+
+    @onlyAccelerator
     @ops(foreach_reduce_op_db)
     @parametrize("w_empty", (False, True))
     def test_foreach_reduce_large_input(self, device, dtype, op, w_empty):
@@ -1940,38 +1962,6 @@ def check_autodiff_sample(op, sample, dtype, is_inplace):
 
 
 instantiate_device_type_tests(TestForeachDevice, globals(), allow_xpu=True)
-
-
-class TestForeachCUDA(TestCase):
-    """CUDA-specific tests for TestForeach."""
-
-    hw_classification = HardwareClassification.CUDA
-
-    @onlyCUDA
-    @dtypes(torch.complex128)
-    def test_foreach_scalarlist_complex_double_many_tensors(self, device, dtype):
-        # Prevent regressions for complex double MTA chunking, see #189827
-        N = 600
-        scalars = [i + 2.0 for i in range(N)]
-
-        def make_tensors():
-            return [
-                torch.full((1, 1, 1), i + 1, dtype=dtype, device=device)
-                for i in range(N)
-            ]
-
-        # out-of-place: depth 2
-        tensors = make_tensors()
-        expect = [t / s for t, s in zip(tensors, scalars)]
-        self.assertEqual(torch.foreach.div(tensors, scalars), expect)
-
-        # in-place: depth 1
-        tensors = make_tensors()
-        torch.foreach.div_(tensors, scalars)
-        self.assertEqual(tensors, expect)
-
-
-instantiate_device_type_tests(TestForeachCUDA, globals(), only_for="cuda")
 
 
 class TestForeachPublicAPI(TestCase):
