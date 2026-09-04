ca9c5ee95590a5b6789608ebc706018b7a742b00
Guilherme Leobas <guilhermeleobas@gmail.com>
2026-09-04 15:16:56 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/_dynamo/variables/dicts.py
+++ torch/_dynamo/variables/dicts.py
@@ -488,7 +488,7 @@ class ConstDictVariable(VariableTracker):
         from .iter import DictIterator
 
         if self.source and not is_constant_source(self.source):
-            install_guard(self.make_guard(GuardBuilder.DICT_KEYS_MATCH))
+            self.install_dict_keys_match_guard()
             tx.output.guard_on_key_order.add(self.source)
         return DictIterator(self.items.keys())
 
