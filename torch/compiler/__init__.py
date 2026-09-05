ccb0590482ea0379341d58fb625dbbefad602c66
Bob Ren <bobren@meta.com>
2026-09-05 16:22:46 +0000

    Update (base update)
    [ghstack-poisoned]

--- torch/compiler/__init__.py
+++ torch/compiler/__init__.py
@@ -26,9 +26,7 @@ from torch._precompile import (
     PrecompileError as PrecompileError,
 )
 
-from . import precompile as precompile
-
-from . import config
+from . import config, precompile as precompile
 from ._cache import CacheInfo
 
 
