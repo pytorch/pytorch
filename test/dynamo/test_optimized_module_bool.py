c8b6c7dcbaddab7711f2c44f04f8ce2287fed70f
NativeMc <yashyt6246@gmail.com>
2026-09-08 19:15:24 +0530

    Test idempotent Dynamo debugger patch installation

--- test/dynamo/test_optimized_module_bool.py
+++ test/dynamo/test_optimized_module_bool.py
@@ -11,6 +11,19 @@ class TestOptimizedModuleBool(TestCase):
         compiled = torch.compile(Model(), backend="eager")
         self.assertTrue(bool(compiled))
 
+    def test_debugger_fix_installation_is_idempotent(self):
+        from torch._dynamo import _bytecode_debugger_fix
+        from torch._dynamo import bytecode_debugger, eval_frame
+
+        debug_init = bytecode_debugger._DebugContext.__init__
+        optimized_bool = eval_frame.OptimizedModule.__bool__
+        _bytecode_debugger_fix._init
+        bytecode_debugger._DebugContext._native_neo_debugger_fix_installed = True
+        eval_frame.OptimizedModule._native_neo_bool_fix_installed = True
+        _bytecode_debugger_fix.__dict__
+        self.assertIs(bytecode_debugger._DebugContext.__init__, debug_init)
+        self.assertIs(eval_frame.OptimizedModule.__bool__, optimized_bool)
+
 
 if __name__ == "__main__":
     torch.testing._internal.common_utils.run_tests()
