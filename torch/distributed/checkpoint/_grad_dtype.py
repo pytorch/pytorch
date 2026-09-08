f2c379384fe07e483d7a42f487450ba38e8c97e6
NativeMc <yashyt6246@gmail.com>
2026-09-08 11:54:20 +0530

    Correct DCP grad_dtype state initialization

--- torch/distributed/checkpoint/_grad_dtype.py
+++ torch/distributed/checkpoint/_grad_dtype.py
@@ -1,31 +1,38 @@
 import torch
 
-from . import state_dict as _state_dict
-
-
-_original = _state_dict._init_optim_state
-
 
 def _init_optim_state(optim: torch.optim.Optimizer) -> None:
+    if optim.state:
+        return
+
     for param_group in optim.param_groups:
         for param in param_group["params"]:
-            if param.requires_grad and param.grad is None:
+            if param.grad is not None:
+                return
+
+    for param_group in optim.param_groups:
+        for param in param_group["params"]:
+            if param.requires_grad:
                 grad_dtype = getattr(param, "grad_dtype", None)
                 if grad_dtype is None:
                     grad_dtype = param.dtype
                 param.grad = torch.zeros_like(param, dtype=grad_dtype)
 
-    original_grads = []
+    lrs = []
     for param_group in optim.param_groups:
-        for param in param_group["params"]:
-            original_grads.append((param, param.grad))
+        if "lr" in param_group:
+            lrs.append(param_group["lr"])
+            param_group["lr"] = (
+                torch.tensor(0.0)
+                if isinstance(param_group["lr"], torch.Tensor)
+                else 0.0
+            )
+    optim.step(closure=None)
 
-    try:
-        return _original(optim)
-    finally:
-        for param, grad in original_grads:
-            if grad is None:
-                param.grad = None
+    for param_group in optim.param_groups:
+        if "lr" in param_group:
+            param_group["lr"] = lrs.pop(0)
+    optim.zero_grad(set_to_none=True)
 
 
 __all__ = ["_init_optim_state"]
