291e47107d7a3ede420b661a0acf417b5cab40bb
NativeMc <yashyt6246@gmail.com>
2026-09-08 11:52:14 +0530

    Test optimizer state isolation

---
+++ test/optim/test_optimizer_state_isolation.py
@@ -0,0 +1,27 @@
+import torch
+from torch.testing._internal.common_utils import TestCase
+
+
+class TestOptimizerStateIsolation(TestCase):
+    def test_load_state_dict_clones_tensor_state(self):
+        p1 = torch.nn.Parameter(torch.ones(2))
+        p2 = torch.nn.Parameter(torch.ones(2))
+        opt1 = torch.optim.SGD([p1], lr=0.1, momentum=0.9)
+        opt2 = torch.optim.SGD([p2], lr=0.1, momentum=0.9)
+
+        p1.grad = torch.ones_like(p1)
+        opt1.step()
+        opt2.load_state_dict(opt1.state_dict())
+
+        state1 = opt1.state[p1]["momentum_buffer"]
+        state2 = opt2.state[p2]["momentum_buffer"]
+        self.assertNotEqual(state1.data_ptr(), state2.data_ptr())
+
+        before = state1.clone()
+        p2.grad = torch.full_like(p2, 2.0)
+        opt2.step()
+        torch.testing.assert_close(state1, before)
+
+
+if __name__ == "__main__":
+    torch.testing._internal.common_utils.run_tests()
