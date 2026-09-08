4364ac5f1e469af8b7d2041006390575ac923140
NativeMc <yashyt6246@gmail.com>
2026-09-08 18:36:29 +0530

    Test DCP preserves parameter grad flags

--- test/distributed/checkpoint/test_optimizer_frozen_state.py
+++ test/distributed/checkpoint/test_optimizer_frozen_state.py
@@ -26,6 +26,25 @@ class TestDCPFrozenOptimizerState(TestCase):
 
         self.assertEqual(set(opt.state_dict()["state"]), set(loaded.state_dict()["state"]))
 
+    def test_load_preserves_requires_grad(self):
+        model = torch.nn.Sequential(
+            torch.nn.Linear(2, 2, bias=False),
+            torch.nn.Linear(2, 2, bias=False),
+        )
+        opt = torch.optim.AdamW(model.parameters())
+        sum(param.sum() for param in model.parameters()).backward()
+        opt.step()
+        state = get_optimizer_state_dict(model, opt)
+
+        first, second = model.parameters()
+        first.requires_grad_(False)
+        second.requires_grad_(True)
+        loaded = torch.optim.AdamW(model.parameters())
+        set_optimizer_state_dict(model, loaded, state)
+
+        self.assertFalse(first.requires_grad)
+        self.assertTrue(second.requires_grad)
+
 
 if __name__ == "__main__":
     torch.testing._internal.common_utils.run_tests()
