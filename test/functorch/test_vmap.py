6c44685425dc15a58fe0b13af4aae98f7be3442e
Piotr Bielak <piotr.bielak@intel.com>
2026-09-10 13:34:17 +0000

    Add `hw_classification` to `TestVmapAPI`

--- test/functorch/test_vmap.py
+++ test/functorch/test_vmap.py
@@ -64,6 +64,7 @@ from torch.testing._internal.common_device_type import (
 )
 from torch.testing._internal.common_methods_invocations import op_db
 from torch.testing._internal.common_utils import (
+    HardwareClassification,
     instantiate_parametrized_tests,
     IS_WINDOWS,
     markDynamoStrictTest,
@@ -109,6 +110,8 @@ class EnableVmapFallbackWarnings:
 
 @markDynamoStrictTest
 class TestVmapAPI(TestCase):
+    hw_classification = HardwareClassification.GENERIC
+
     def test_non_tensor_output_raises(self):
         with self.assertRaisesRegex(ValueError, "got type <class 'float'>"):
             vmap(lambda x: 3.14)(torch.ones(3))
