39d4c41cd81c110e39e3328c9cfb8408311b22e0
gplutop7 <grzegorzx.pluto-prondzinski@intel.com>
2026-09-11 16:52:17 +0000

    test_linalg: skip Dynamo-incompatible error-path tests

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -3315,6 +3315,7 @@ class TestLinalgDevice(TestLinalg):
         not TEST_WITH_ROCM and _get_torch_cuda_version() < (12, 8) and not torch.cuda.has_magma,
         "torch.linalg.eig requires MAGMA for CUDA versions < 12.8",
     )
+    @skipIfTorchDynamo("Dynamo wraps RuntimeError in TorchDynamoException during compiled autograd")
     @dtypes(torch.complex128)
     def test_invariance_error_spectral_decompositions(self, device, dtype):
         make_arg = partial(make_tensor, device=device, dtype=dtype, requires_grad=True)
@@ -3694,6 +3695,7 @@ class TestLinalgDevice(TestLinalg):
 
     @skipCUDAIfNoMagmaAndNoLinalgsolver
     @skipCPUIfNoLapack
+    @skipIfTorchDynamo("Device check tests interact poorly with Dynamo's tracing")
     @dtypes(*floating_and_complex_types())
     def test_pinv_errors_and_warnings(self, device, dtype):
         # pinv requires at least 2D tensor
@@ -5590,6 +5592,7 @@ class TestLinalgDevice(TestLinalg):
 
     @skipCPUIfNoLapack
     @skipCUDAIfNoCusolver
+    @skipIfTorchDynamo("numpy.linalg.qr(mode='raw') is not supported by FakeTensor meta dispatcher")
     @dtypes(*floating_and_complex_types())
     def test_householder_product(self, device, dtype):
         def generate_reflectors_and_tau(A):
@@ -5651,6 +5654,7 @@ class TestLinalgDevice(TestLinalg):
             run_test(shape)
 
     @skipCPUIfNoLapack
+    @skipIfTorchDynamo("Device check tests interact poorly with Dynamo's tracing")
     def test_householder_product_errors_and_warnings(self, device):
         test_cases = [
             # input1 size, input2 size, error regex
