1fe0abd1d7a1166b973d366986655b559a2d9511
gplutop7 <grzegorzx.pluto-prondzinski@intel.com>
2026-09-11 16:52:12 +0000

    test_linalg: fix NumPy squeez

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -3365,7 +3365,7 @@ class TestLinalgDevice(TestCase):
             x = torch.linalg.solve(A, b)
             if rhs == ():
                 Ax = np.matmul(A.cpu(), x.unsqueeze(-1).cpu())
-                Ax.squeeze_(-1)
+                Ax = Ax.squeeze(-1)
             else:
                 Ax = np.matmul(A.cpu(), x.cpu())
             self.assertEqual(b.expand_as(Ax), Ax)
