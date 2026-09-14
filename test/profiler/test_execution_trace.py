0b333a1f64d75e2ca831b099ffb88849326e8293
nayan_28 <nkangana@redhat.com>
2026-09-14 17:58:42 +0000

    Changed the classname in instantiate_device_type_tests

--- test/profiler/test_execution_trace.py
+++ test/profiler/test_execution_trace.py
@@ -846,7 +846,7 @@ class TestExecutionTraceDevice(TestCase):
                 raise AssertionError("Expected t2 contents to match [0, 0, 1, 0]")
 
 
-instantiate_device_type_tests(TestExecutionTrace, globals(), allow_xpu=True)
+instantiate_device_type_tests(TestExecutionTraceDevice, globals(), allow_xpu=True)
 
 if __name__ == "__main__":
     run_tests()
