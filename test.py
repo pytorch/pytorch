ae7bc0134debb8fcf79246ae7f103eebffd06b8d
Nikita Vedeneev <nvedzeneyeu@nvidia.com>
2026-09-15 23:08:23 +0000

    cfloat issue -- accumulation problem?

--- test.py
+++ test.py
@@ -4,17 +4,22 @@ torch.manual_seed(42)
 def diff(a, b):
     return (a - b).abs().max()
 
-for n in range(53, 44, -1):
-    for nexp in range(750):
-        x = torch.randn(n, n, dtype=torch.cdouble)
+for n in range(4001, 3999, -1):
+    for nexp in range(20):
+        x = torch.randn(n, n, dtype=torch.cfloat)
         q, _ = torch.linalg.qr(x)
-        s = 2 * torch.rand(n, dtype=torch.double) + 1
+        s = 2 * torch.rand(n, dtype=torch.float) + 1
+        s[::2].mul_(-1)
         x = (q * s.unsqueeze(-2)) @ q.mH
         x = x + x.mH
 
         ld, piv, _ = torch.linalg.ldl_factor_ex(x, hermitian=True)
         l, p, _ = torch.linalg.ldl_factor_ex(x.cuda(), hermitian=True)
 
-        if (err := diff(ld.cuda(), l.tril())) > 1e-12:
+        if diff(piv.cuda(), p) != 0:
+            raise ValueError(f"{n=}, {nexp=}, torch.{x}")
+            breakpoint()
+
+        if (err := diff(ld.cuda(), l.tril())) > 1e-4:
             raise ValueError(f"{err=}, {n=}, {nexp=}, torch.{x}")
             breakpoint()
