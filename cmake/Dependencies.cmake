a1461240ea384ef3aacaf724b30b22aa7d898b8d
Z Liu <zhuojin.liu@amd.com>
2026-09-15 19:56:27 +0000

    stop linking amdsmi on windows

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1149,7 +1149,7 @@ if(USE_ROCM)
     set(Caffe2_PUBLIC_HIP_DEPENDENCY_LIBS
       hip::host MIOpen hiprtc::hiprtc)
 
-    if(ROCM_VERSION_DEV VERSION_GREATER_EQUAL "7.14.0")
+    if(ROCM_VERSION_DEV VERSION_GREATER_EQUAL "7.14.0" AND amd_smi_FOUND)
       list(APPEND Caffe2_HIP_DEPENDENCY_LIBS amd_smi)
     endif()
 
