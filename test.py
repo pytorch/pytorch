4e3420a58b19788336f818b7fabf82b0c9b6328a
Nikita Vedeneev <nvedzeneyeu@nvidia.com>
2026-09-15 23:17:12 +0000

    cfloat fine -- just  pivot divergence

--- test.py
+++ test.py
@@ -12,14 +12,18 @@ for n in range(4001, 3999, -1):
         s[::2].mul_(-1)
         x = (q * s.unsqueeze(-2)) @ q.mH
         x = x + x.mH
+        x_cuda = x.cuda()
 
-        ld, piv, _ = torch.linalg.ldl_factor_ex(x, hermitian=True)
         l, p, _ = torch.linalg.ldl_factor_ex(x.cuda(), hermitian=True)
-
-        if diff(piv.cuda(), p) != 0:
-            raise ValueError(f"{n=}, {nexp=}, torch.{x}")
-            breakpoint()
-
-        if (err := diff(ld.cuda(), l.tril())) > 1e-4:
+        sol = torch.linalg.ldl_solve(l.cpu(), p.cpu(), (x_cuda @ x_cuda).cpu(), hermitian=True)
+        if (err := diff(sol.cuda(), x_cuda)) > 1e-4:
             raise ValueError(f"{err=}, {n=}, {nexp=}, torch.{x}")
             breakpoint()
+
+        #if diff(piv.cuda(), p) != 0:
+        #    raise ValueError(f"{n=}, {nexp=}, torch.{x}")
+        #    breakpoint()
+
+        #if (err := diff(ld.cuda(), l.tril())) > 1e-4:
+        #    raise ValueError(f"{err=}, {n=}, {nexp=}, torch.{x}")
+        #    breakpoint()
