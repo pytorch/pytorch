24e1b81177c51614290e7e4594689f4e4dc6a23b
Angel Li <liangel@meta.com>
2026-09-16 14:39:49 -0700

    modifying tests in prep for c++ faketensor
    ghstack-source-id: 75dcda27e25c8cbdd873f1b8563dce05c383955d
    Pull-Request: https://github.com/pytorch/pytorch/pull/191798

--- test/test_mkldnn.py
+++ test/test_mkldnn.py
@@ -22,7 +22,8 @@ import torch.backends.mkldnn
 from torch.utils import mkldnn as mkldnn_utils
 from torch.testing._internal.common_utils import TestCase, \
     run_tests, TemporaryFileName, gradcheck, gradgradcheck, IS_WINDOWS, \
-    skipIfTorchDynamo, xfailIfTorchDynamo, recover_orig_fp32_precision
+    expectedIfCppFakeTensor, skipIfTorchDynamo, xfailIfTorchDynamo, \
+    recover_orig_fp32_precision
 from torch.testing._internal.common_device_type import (
     instantiate_device_type_tests,
     dtypes,
@@ -870,7 +871,7 @@ class TestMkldnn(TestCase):
                 self.assertEqual(y1, y2.to_dense())
 
     # https://github.com/pytorch/pytorch/issues/127111
-    @xfailIfTorchDynamo
+    @expectedIfCppFakeTensor(lambda fn: fn, xfailIfTorchDynamo)
     def test_max_pool_unsupported(self):
         # OneDNN not support dilation max_pooling, will be avilabled in v2.0.
         N = torch.randint(3, 10, (1,)).item()
