4fdb0e494ec10ab33772df1a7ef35482c008f8d9
syg1996419 <18738121386@163.com>
2026-09-16 16:40:25 +0800

    [Testcase Refactoring] Move test_time_mm_fuse to TestMinFunctorchOnly instead of TestMinDevice

--- test/functorch/test_dims.py
+++ test/functorch/test_dims.py
@@ -418,6 +418,40 @@ class TestMin(TestBase):
         C = (A[i, k] * B[k, j]).sum(k).order(i, j)
         torch.testing.assert_close(C, A @ B)
 
+    def test_time_mm_fuse(self):
+        i, j, k = dims()
+        A = torch.rand(3, 4)
+        B = torch.rand(4, 5)
+
+        for _ in range(10):
+            r0 = A @ B
+
+        for _ in range(10):
+            a = A[i, k]
+            b = B[k, j]
+            r1 = (a * b).sum(k)
+
+        with measure("pp"):
+            for _ in range(10000):
+                A @ B
+        # magic_trace_stop_indicator()
+
+        with measure("fc"):
+            for _ in range(10000):
+                (A[i, k] * B[k, j]).sum(k).order(i, j)
+
+        with magic_trace("f.fxt"):
+            for _ in range(10000):
+                (A[i, k] * B[k, j]).sum(k).order(i, j)
+
+        with magic_trace("p.fxt"):
+            for _ in range(10000):
+                A @ B
+
+        # magic_trace_stop_indicator()
+
+        torch.testing.assert_close(r1.order(i, j), r0)
+
     def test_compare_dims(self):
         i, j = dims()
         i.size = 3
@@ -655,10 +689,25 @@ class TestMinDevice(TestBase):
             linear=torch.nn.Linear,
         )
 
-    def test_time_mm_fuse(self, device):
+
+instantiate_device_type_tests(TestMinDevice, globals(), except_for=("cpu",))
+
+
+class TestMinFunctorchOnly(TestMin):
+    hw_classification = HardwareClassification.GENERIC
+
+    def setUp(self):
+        super().setUp()
+        functorch.dim.POINTWISE_OPTIMIZE = False
+
+    def tearDown(self):
+        functorch.dim.POINTWISE_OPTIMIZE = True
+        super().tearDown()
+
+    def test_time_mm_fuse(self):
         i, j, k = dims()
-        A = torch.rand(3, 4, device=device)
-        B = torch.rand(4, 5, device=device)
+        A = torch.rand(3, 4)
+        B = torch.rand(4, 5)
 
         for _ in range(10):
             r0 = A @ B
@@ -688,21 +737,6 @@ class TestMinDevice(TestBase):
         # magic_trace_stop_indicator()
 
         torch.testing.assert_close(r1.order(i, j), r0)
-
-
-instantiate_device_type_tests(TestMinDevice, globals(), except_for=("cpu",))
-
-
-class TestMinFunctorchOnly(TestMin):
-    hw_classification = HardwareClassification.GENERIC
-
-    def setUp(self):
-        super().setUp()
-        functorch.dim.POINTWISE_OPTIMIZE = False
-
-    def tearDown(self):
-        functorch.dim.POINTWISE_OPTIMIZE = True
-        super().tearDown()
 
 
 if __name__ == "__main__":
