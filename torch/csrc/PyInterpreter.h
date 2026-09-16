c1b727c9e1179db0a2c0ecee7bd1615b3d9f55d1
Angel Li <liangel@meta.com>
2026-09-16 12:35:58 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/PyInterpreter.h
+++ torch/csrc/PyInterpreter.h
@@ -19,5 +19,5 @@ TORCH_PYTHON_API void initializeGlobalPyInterpreter();
 // get the python CppFakeTensorMode object for mode, minting a fresh wrapper
 // around the same C++ mode if the previous one has been collected. Returns None
 // only when mode is null.
-TORCH_PYTHON_API py::object getFakeModePyObj(
+TORCH_PYTHON_API py::object getCppFakeTensorModePyObj(
     const std::shared_ptr<c10::FakeTensorMode>& mode);
