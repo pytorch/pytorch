41e0c404b1645f001f6b60c025a4ac9e1087e8de
Yu, Guangye <guangye.yu@intel.com>
2026-09-16 18:08:23 +0000

    [UPDATE] Update
    [ghstack-poisoned]

--- cmake/Modules/FindSYCLToolkit.cmake
+++ cmake/Modules/FindSYCLToolkit.cmake
@@ -184,6 +184,7 @@ elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
     PATH_SUFFIXES include
     )
 endif()
+message(FATAL_ERROR ${LevelZero_INCLUDE_DIR} ", "  $ENV{LEVEL_ZERO_V1_SDK_PATH})
 
 if(NOT LevelZero_INCLUDE_DIR)
   set(SYCL_FOUND False)
