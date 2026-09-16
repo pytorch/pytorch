9085440214c0965b3a88707dd4080719fbfbe789
Benedykt Bela <benedykt.bela@intel.com>
2026-09-16 12:33:27 +0000

    Rename file.
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- .ci/pytorch/multigpu-test.sh
+++ .ci/pytorch/multigpu-test.sh
@@ -15,7 +15,7 @@ if [[ "${SHARD_NUMBER:-1}" == "1" ]]; then
 fi
 
 if [[ "${SHARD_NUMBER:-2}" == "2" ]]; then
-    time python test/run_test.py --include test_cuda_multigpu test_cuda_primary_ctx --verbose
+    time python test/run_test.py --include test_multigpu test_cuda_primary_ctx --verbose
 
     # Disabling tests to see if they solve timeout issues; see https://github.com/pytorch/pytorch/issues/70015
     # python tools/download_mnist.py --quiet -d test/cpp/api/mnist
