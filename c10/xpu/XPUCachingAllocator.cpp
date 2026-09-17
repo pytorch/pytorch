c18f9bf9b9c17f44e4a9fca8b177a12e504ffd77
Yu, Guangye <guangye.yu@intel.com>
2026-09-17 16:48:55 +0000

    Update (base update)
    [ghstack-poisoned]

--- c10/xpu/XPUCachingAllocator.cpp
+++ c10/xpu/XPUCachingAllocator.cpp
@@ -1515,20 +1515,8 @@ class DeviceCachingAllocator {
              alloc_block(params, true, context))));
     }
     if (!block_found) {
-      const auto& raw_device = c10::xpu::get_raw_device(device);
-      const auto device_total =
-          raw_device.get_info<sycl::info::device::global_mem_size>();
-      // Estimate the available device memory when the SYCL runtime does not
-      // support the corresponding aspect (ext_intel_free_memory).
-      size_t device_free = device_total -
-          stats.reserved_bytes[static_cast<size_t>(StatType::AGGREGATE)]
-              .current;
-      // TODO: Remove the aspect check once the SYCL runtime bug is fixed on
-      // affected devices.
-      if (raw_device.has(sycl::aspect::ext_intel_free_memory)) {
-        device_free =
-            raw_device.get_info<sycl::ext::intel::info::device::free_memory>();
-      }
+      const auto [device_free, device_total] = getMemoryInfo();
+
       std::string allowed_info;
       if (set_fraction) {
         allowed_info = format_size(allowed_memory_maximum) + " allowed; ";
@@ -1849,7 +1837,32 @@ class DeviceCachingAllocator {
         "to help us prioritize its implementation.");
     const size_t free =
         device.get_info<sycl::ext::intel::info::device::free_memory>();
+
+#if SYCL_COMPILER_VERSION >= 20260200
+    const auto arch = device.get_info<sycl::info::device::architecture>();
+    if (arch <
+        sycl::ext::oneapi::experimental::architecture::intel_gpu_bmg_g21) {
+      return {free, total};
+    }
+    // See
+    // https://github.com/intel/compute-runtime/blob/master/programmers-guide/DEVICE_MEMORY_ACCOUNTING.md#umd-headroom.
+    constexpr double kIntegratedGpuUsableFraction = 0.94;
+#ifdef _WIN32
+    constexpr double kDiscreteGpuUsableFraction = 0.98;
+#else
+    constexpr double kDiscreteGpuUsableFraction = 0.95;
+#endif
+    const double usable_fraction =
+        device.has(sycl::aspect::ext_oneapi_is_integrated_gpu)
+        ? kIntegratedGpuUsableFraction
+        : kDiscreteGpuUsableFraction;
+    const size_t free_adjust = free + (1 - usable_fraction) * total;
+    TORCH_CHECK(
+        free_adjust <= total, "Calculated free memory exceeds total memory.");
+    return {free_adjust, total};
+#else
     return {free, total};
+#endif
   }
 
   double getMemoryFraction() {
