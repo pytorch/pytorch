82c6755a3c1e5b6f67bdb4fcefdfb6c6378c04c5
zhaiyaxing001 <616636986@qq.com>
2026-09-17 20:51:15 +0800

    lint format

--- test/distributed/_composable/test_replicate_with_compiler.py
+++ test/distributed/_composable/test_replicate_with_compiler.py
@@ -39,6 +39,7 @@ from torch.testing._internal.distributed.fake_pg import FakeStore
 from torch.testing._internal.inductor_utils import HAS_GPU
 from torch.utils.checkpoint import checkpoint
 
+
 DIM = 2000
 
 
