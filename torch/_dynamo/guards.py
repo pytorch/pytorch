a3c2c5cc4213bd37737c9f6a9946d8661199f40d
dsashidh <dsashidh@redhat.com>
2026-09-17 20:40:37 +0000

    improve messaging for SET_CONTAINS

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -2516,11 +2516,15 @@ class GuardBuilder(GuardBuilderBase):
             return
 
         self._set_guard_export_info(guard, [code])
+        reason = (
+            f"Set {set_ref} must contain item {item!r}; Dynamo specialized the "
+            "compiled code on this item being present."
+        )
 
         self.get_guard_manager(guard).add_set_contains_guard(
             True,
             item,
-            get_verbose_code_parts(code, guard),
+            get_verbose_code_parts(reason, guard),
             guard.user_stack,
         )
         self.already_added_code_parts.add(code)
@@ -2538,11 +2542,15 @@ class GuardBuilder(GuardBuilderBase):
             return
 
         self._set_guard_export_info(guard, [code])
+        reason = (
+            f"Set {set_ref} must not contain item {item!r}; Dynamo specialized the "
+            "compiled code on this item being absent."
+        )
 
         self.get_guard_manager(guard).add_set_contains_guard(
             False,
             item,
-            get_verbose_code_parts(code, guard),
+            get_verbose_code_parts(reason, guard),
             guard.user_stack,
         )
         self.already_added_code_parts.add(code)
