2998bd8dbb0782f21b048b0359f4a2c918cb37ce
Lakshay Garg <lakshayg@nvidia.com>
2026-09-20 14:02:06 -0700

    Update XPU_HOST_CXX_FLAGS for compatibility with add_compile_definitions

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -88,7 +88,7 @@ if(USE_XPU)
     caffe2_update_option(USE_XPU OFF)
   endif()
   foreach(flag ${XPU_HOST_CXX_FLAGS})
-    add_definitions(${flag})
+    add_compile_definitions(${flag})
   endforeach()
 endif()
 
