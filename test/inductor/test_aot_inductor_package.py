8d75835fbfdc2d9289704022969eb046e6e27dc0
rraminen_amdeng <RamyaSai.Ramineni@amd.com>
2026-09-22 17:17:22 +0000

    Remove unused imports and lint fixes

--- test/inductor/test_aot_inductor_package.py
+++ test/inductor/test_aot_inductor_package.py
@@ -29,15 +29,10 @@ from torch.export.pt2_archive._package import (
     load_pt2,
     load_weights_to_pt2_contents,
 )
-from torch.testing._internal.common_cuda import (
-    _get_torch_cuda_version,
-    requires_triton_ptxas_compat,
-    TRITON_PTXAS_VERSION,
-)
+from torch.testing._internal.common_cuda import requires_triton_ptxas_compat
 from torch.testing._internal.common_utils import (
     HardwareClassification,
     IS_FBCODE,
-    TEST_CUDA,
 )
 from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_GPU
 from torch.utils import _pytree as pytree
