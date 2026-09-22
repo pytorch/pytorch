b2ba491ef73d54daba3a9756126117133aefd493
Wang, Chuanqi <chuanqi.wang@intel.com>
2026-09-22 21:36:18 +0800

    TestFX.test_shape_prop_layout_3d can pass on Neoverse-V2

--- test/test_fx.py
+++ test/test_fx.py
@@ -75,11 +75,15 @@ from torch.testing._internal.common_utils import (
     find_library_location,
     IS_FBCODE,
     IS_MACOS,
+    IS_ARM64,
+    IS_LINUX,
+    IS_CPU_CAPABILITY_SVE256,
     IS_WINDOWS,
     TEST_WITH_CROSSREF,
     TEST_WITH_ROCM,
     run_tests,
     skipIfTorchDynamo,
+    xfailIf,
     xfailIfNoAcceleratorTriton,
 )
 from torch.testing._internal.jit_utils import JitTestCase
@@ -2261,7 +2265,7 @@ def forward(self, x : _torch_Tensor_) -> _torch_Tensor_:
                         f"Expected tensor_meta[1].shape == torch.Size([]), "
                         f"got {tensor_meta[1].shape}"
                     )
-
+    @xfailIf(IS_ARM64 and IS_LINUX and IS_CPU_CAPABILITY_SVE256) # RuntimeError: label is too far
     def test_shape_prop_layout_3d(self):
         class ConvTest3d(torch.nn.Module):
             def __init__(self) -> None:
