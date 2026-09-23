b08f6db52b4bc1e355d5bbebea14182dac2f6121
Angel Li <liangel@meta.com>
2026-09-23 15:26:39 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/nn/parameter.py
+++ torch/nn/parameter.py
@@ -51,7 +51,9 @@ class Parameter(torch.Tensor, metaclass=_ParameterMeta):
     def __new__(cls, data=None, requires_grad=True):
         if data is None:
             data = torch.empty(0)
-        if type(data) is torch.Tensor or type(data) is Parameter:
+        if (
+            type(data) is torch.Tensor or type(data) is Parameter
+        ) and not torch._C._is_fake_tensor(data):
             # For ease of BC maintenance, keep this path for standard Tensor.
             # Eventually (tm), we should change the behavior for standard Tensor to match.
             return torch.Tensor._make_subclass(cls, data, requires_grad)
