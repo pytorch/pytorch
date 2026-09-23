591b890f168339f70cdd4c6fea455a553c4095e3
ethanwee1 <ethanwee@amd.com>
2026-09-23 17:38:21 +0000

    [ROCm][CI] Detect torchcomms test build architecture
    Keep test jobs on runtime detection while supplying Torch CMake the explicit ROCm target required to build torchcomms.

--- .ci/pytorch/common_utils.sh
+++ .ci/pytorch/common_utils.sh
@@ -333,7 +333,18 @@ function install_torchcomms() {
   else
     export USE_NCCL=0
   fi
-  pip_build_and_install "git+https://github.com/meta-pytorch/torchcomms.git@${commit}" dist/torchcomms
+  if [[ "${BUILD_ENVIRONMENT}" == *rocm* && -z "${PYTORCH_ROCM_ARCH:-}" ]]; then
+    local runtime_rocm_arch
+    runtime_rocm_arch=$(rocminfo | awk '$1 == "Name:" && $2 ~ /^gfx[0-9]/ {print $2}' | sort -u | paste -sd';' -)
+    if [[ -z "${runtime_rocm_arch}" ]]; then
+      echo "Could not detect a ROCm architecture for the torchcomms build" >&2
+      return 1
+    fi
+    PYTORCH_ROCM_ARCH="${runtime_rocm_arch}" \
+      pip_build_and_install "git+https://github.com/meta-pytorch/torchcomms.git@${commit}" dist/torchcomms
+  else
+    pip_build_and_install "git+https://github.com/meta-pytorch/torchcomms.git@${commit}" dist/torchcomms
+  fi
 }
 
 function install_spmd_types() {
