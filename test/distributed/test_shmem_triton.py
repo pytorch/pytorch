c32fc11314f026a45533e0636c8f0f3e1fa9f693
Aaron Gokaslan <aaronGokaslan@gmail.com>
2026-09-25 12:42:18 -0700

    [NVSHMEM] Use header-derived signal ops in Triton tests

--- test/distributed/test_shmem_triton.py
+++ test/distributed/test_shmem_triton.py
@@ -447,7 +447,12 @@ class SHMEMTritonTest(MultiProcContinuousTest):
         flag = out_hdl.get_signal_pad(rank, (1,), dtype=torch.int64).fill_(0)
 
         peer = 1 - rank
-        NVSHMEM_SIGNAL_SET = 0 if TEST_WITH_ROCM else 9
+        if TEST_WITH_ROCM:
+            NVSHMEM_SIGNAL_SET = 0
+        else:
+            from torch._C._distributed_c10d import _nvshmem_signal_op_values
+
+            NVSHMEM_SIGNAL_SET, _ = _nvshmem_signal_op_values()
         SIGNAL_VAL = 1  # Signal completion value
         NVSHMEM_CMP_EQ = 0  # compare equal for signal wait until
 
@@ -506,9 +511,12 @@ class SHMEMTritonTest(MultiProcContinuousTest):
         flag = out_hdl.get_signal_pad(rank, (1,), dtype=torch.int64).fill_(0)
 
         peer = 1 - rank
-        # NVSHMEM_SIGNAL_ADD = 10 (NVSHMEM 3.8 nvshmemx_signal_op_t)
-        # ROCSHMEM_SIGNAL_ADD = 1 (rocshmem_common.hpp enum ROCSHMEM_SIGNAL_OPS)
-        NVSHMEM_SIGNAL_ADD = 1 if TEST_WITH_ROCM else 10
+        if TEST_WITH_ROCM:
+            NVSHMEM_SIGNAL_ADD = 1
+        else:
+            from torch._C._distributed_c10d import _nvshmem_signal_op_values
+
+            _, NVSHMEM_SIGNAL_ADD = _nvshmem_signal_op_values()
         SIGNAL_VAL = 16  # val + NVSHMEM_SIGNAL_ADD
         NVSHMEM_CMP_EQ = 0
 
